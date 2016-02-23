86def8fdd7283883d55a9abc62e939512a8f9786
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-23 17:45:22 +0000

    Added specific peep test and code

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
+require 'json'
 require_relative 'data_mapper_setup'
 
 class ChitterApi < Sinatra::Base
@@ -11,6 +12,13 @@ class ChitterApi < Sinatra::Base
   get '/peeps' do
     content_type :json
     Peep.all.to_json
+  end
+
+  get '/peeps/:id' do
+    content_type :json
+    peep = Peep.get(params[:id])
+    return status 404 if peep.nil?
+    peep.to_json
   end
 
   run! if app_file == $PROGRAM_NAME
--- spec/features/get_peep_data_spec.rb
+++ spec/features/get_peep_data_spec.rb
@@ -1,11 +1,24 @@
 describe 'Getting peeps' do
-  let(:user) { user = create(:user) }
+
+  before(:all) do
+    user = create(:user)
+    create(:peep, user: user)
+    create(:peep, user: user)
+  end
 
   it 'allows user to get peeps' do
-    create(:peep, user: user)
-    create(:peep, user: user)
     get '/peeps'
     expect_json_types('*', :array_of_objects)
     expect_json_types('*.', id: :int, message: :string)
+  end
+
+  it 'allows user to get specific peep' do
+    get '/peeps/2'
+    expect_json(id: 2)
+  end
+
+  it 'receives status 404 if peep does not exist' do
+    get '/peeps/3'
+    expect_status(404)
   end
 end
